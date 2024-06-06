c55092c2993e19a594efc8432a0ffab2f9b42eb6
kyimmQ <cacphuong.le@gmail.com>
2024-06-06 13:51:39 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+      flash[:success] = "Welcome #{@user.name} to our app!"
+      redirect_to user_url(@user)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # disallow unwanted field
+  def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
